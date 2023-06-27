8b32d7f6218fc215c850a8318eedd164ecc1e0bc
Sotheavuth Nguon <sotheavuth.nguon@yahoo.com>
2023-06-27 16:33:06 +0700

    Updated

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SimpleNetworkingSDK",
-            targets: ["SimpleNetworkingSDK"]),
+            targets: ["SimpleNetworkingSDK"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,11 +19,13 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "SimpleNetworkingSDK",
-            dependencies: []),
+        .binaryTarget(
+          name: "SimpleNetworkingSDK",
+          path: "Sources/SimpleNetworkingSDK/SimpleNetworkingSDK.xcframework"
+        ),
         .testTarget(
             name: "SimpleNetworkingSDKTests",
-            dependencies: ["SimpleNetworkingSDK"]),
+            dependencies: ["SimpleNetworkingSDK"]
+        ),
     ]
 )
--- Sources/SimpleNetworkingSDK/SimpleNetworkingSDK.swift
+++
@@ -1,6 +0,0 @@
-public struct SimpleNetworkingSDK {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
